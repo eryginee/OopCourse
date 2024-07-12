9e64f9470078af8d6227c10284a50dbf35832663
eryginee <eerygin94@gmail.com>
2024-07-12 14:23:38 +0700

    Write code

--- Range/src/eryginee/range_main/Main.java
+++ Range/src/eryginee/range_main/Main.java
@@ -1,7 +1,35 @@
 package eryginee.range_main;
 
+import java.util.Scanner;
+import eryginee.range.Range;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+        Range range = new Range(10.45, 27.68);
+
+        System.out.println("Длина диапазона чисел равна: " + range.getLength());
+
+        System.out.println("Введите число для проверки:");
+        double enteredNumber = scanner.nextDouble();
+
+        if (range.isInside(enteredNumber)) {
+            System.out.printf("Число %.2f входит в установленный диапазон%n", enteredNumber);
+        } else {
+            System.out.printf("Число %.2f не входит в установленный диапазон%n", enteredNumber);
+        }
+
+        System.out.println();
+
+        range.setFrom(35.54);
+        range.setTo(57.93);
+
+        System.out.println("Длина измененного диапазона чисел равна: " + range.getLength());
+
+        if (range.isInside(enteredNumber)) {
+            System.out.printf("Число %.2f входит в измененный диапазон%n", enteredNumber);
+        } else {
+            System.out.printf("Число %.2f не входит в измененный диапазон%n", enteredNumber);
+        }
     }
 }
